cecf63390ff915fbbfa2c7d9ba17a73d36da96c1
Sjoerd Lemkens <97674344+sjoerdlemkens@users.noreply.github.com>
2026-06-12 12:34:00 +0200

    chore: init

---
+++ PlatziStore/PlatziStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziStoreApp.swift
+//  PlatziStore
+//
+//  Created by Sjoerd 2 on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlatziStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
